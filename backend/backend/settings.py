358a010e2438aad3415a3f91045a8e7d9c28f7c4
Crschnicker <129310444+Crschnicker@users.noreply.github.com>
2024-12-19 13:23:36 -0800

    V1.7.25
    Longer Token No Blacklist for Dev Testing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,10 +42,10 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": True,
     
     "ALGORITHM": "HS256",
